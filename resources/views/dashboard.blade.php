90fe1b3e871ff26edc36dfc2d7bdf05b274e459c
baranc44 <baran@de-it.nl>
2022-02-23 15:26:11 +0100

    updated login form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
-            <p>Test</p>
         </div>
     </div>
 </x-app-layout>
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="img/menu-logo.png" alt="Feest 4 all logo" width="450">
+</a>
