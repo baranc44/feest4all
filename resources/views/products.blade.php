1cec08c2590983ebee163bbcc48006ecf771d135
404 spoon <jordy@de-it.nl>
2022-02-23 16:40:53 +0100

    removed

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,9 +10,6 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div>
-                        <x-jet-application-logo class="block h-12 w-auto" />
-                    </div>
                 
                     <div class="mt-8 text-2xl">
                         Welcome to your Jetstream application!
