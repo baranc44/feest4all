479b3d7c26de17186d82b5ad0161b8c6ef859b04
404 spoon <jordy@de-it.nl>
2022-02-24 10:26:44 +0100

    updated product design

--- resources/views/producten.blade.php
+++ resources/views/producten.blade.php
@@ -10,17 +10,23 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                
-                    <div class="mt-8 text-2xl">
-                        Welcome to your Jetstream application!
-                    </div>
-                
-                    <div class="mt-6 text-gray-500">
-                        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                        ecosystem to be a breath of fresh air. We hope you love it.
-                    </div>
+                    <table>
+                        <tr class="mt-8 text-2xl w-100%">
+                            <th class="w-25 p-3">Product naam</th>
+                            <th class="w-25 p-3">Voorraad</th>
+                            <th class="w-25 p-3">Prijs/Eenheid</th>
+                            <th class="w-25 p-3">Eenheid</th>
+                            <th class="w-25 p-3">action</th>
+                        </tr>
+                    
+                        <tr class="mt-6 text-gray-500">
+                            <td>Product</td>
+                            <td>Product</td>
+                            <td>Product</td>
+                            <td>Product</td>
+                            <td>Product</td>
+                        </tr>
+                    </table>
                 </div>
                 
                 
