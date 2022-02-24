9bcd00b2a7f4dc73faa706f5871b9d88c844eaf3
404 spoon <jordy@de-it.nl>
2022-02-24 13:25:11 +0100

    made product table nicer

--- resources/views/producten.blade.php
+++ resources/views/producten.blade.php
@@ -7,16 +7,23 @@
     <div class="py-12">
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">       
-                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200 text-left">
                     <table>
                         <tr class="mt-8 text-2xl">
-                            <th class="w-1/5">Product naam</th>
-                            <th class="w-1/5">Voorraad</th>
-                            <th class="w-1/5">Prijs/Eenheid</th>
-                            <th class="w-1/5">Eenheid</th>
-                            <th class="w-1/5">action</th>
+                            <th class="w-1/4">Product naam</th>
+                            <th class="w-1/4">Voorraad</th>
+                            <th class="w-1/4">Prijs/Eenheid</th>
+                            <th class="w-1/4">Eenheid</th>
+                            <th class="w-1/4">action</th>
                         </tr>
                     
+                        <tr class="mt-6 text-gray-500">
+                            <td>Product</td>
+                            <td>Product</td>
+                            <td>Product</td>
+                            <td>Product</td>
+                            <td>Product</td>
+                        </tr>
                         <tr class="mt-6 text-gray-500">
                             <td>Product</td>
                             <td>Product</td>
