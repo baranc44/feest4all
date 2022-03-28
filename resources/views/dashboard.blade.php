3bbbe254b66bfeb62be83ef1467f7cdb269fa856
baranc44 <baran@de-it.nl>
2022-03-28 15:54:05 +0200

    Update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
